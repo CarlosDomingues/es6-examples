af635753dc350070acf5693304fe6e529b8e8e42
Carlos Felipe Domingues e Oliveira <cWX498295@china.huawei.com>
2018-06-02 17:22:00 -0300

    Added let and const.

--- build/index.bundle.js
+++ build/index.bundle.js
@@ -1,11 +1,23 @@
 "use strict";
 
-var numbers = [1, 2, 3];
+/* let allows you to declare variables that are limited in scope to the block,
+ statement, or expression on which it is used. This is unlike the var keyword, 
+ which defines a variable globally, or locally to an entire function regardless 
+ of block scope.*/
 
-var double = function double(num) {
-  return 2 * num;
-};
+ {
+let x = 1;
 
-var doubleNumArray = numbers.map(double);
+if (x === 1) {
+  let x = 2;
 
-console.log(doubleNumArray);
+  console.log(x);
+  // expected output: 2
+}
+
+console.log(x);
+// expected output: 1
+ }
+
+console.log(x);
+// expected output: ReferenceError: x is not defined
--- src/index.js
+++ src/index.js
@@ -1,8 +1,38 @@
-const numbers = [1,2,3];
+/* let allows you to declare variables that are limited in scope to the block,
+ statement, or expression on which it is used. This is unlike the var keyword, 
+ which defines a variable globally, or locally to an entire function regardless 
+ of block scope.*/
 
-const double = num => 2 * num;
+ {
+let x = 1;
 
-const doubleNumArray = numbers.map(double);
+if (x === 1) {
+  let x = 2;
 
-console.log(doubleNumArray);
+  console.log(x);
+  // expected output: 2
+}
 
+console.log(x);
+// expected output: 1
+ }
+
+console.log(x);
+// expected output: ReferenceError: x is not defined
+
+/* Constants are block-scoped, much like variables defined using the let 
+statement. The value of a constant cannot change through re-assignment, and it
+ can't be redeclared.*/
+
+ const number = 42;
+
+try {
+  number = 99;
+} catch(err) {
+  console.log(err);
+  // expected output: TypeError: invalid assignment to const `number'
+  // Note - error messages will vary depending on browser
+}
+
+console.log(number);
+// expected output: 42
